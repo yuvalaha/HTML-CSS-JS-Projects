e3d2e13eeaaa596d38b61f16c4466d216cd87eb4
yuvalaha <144541073+yuvalaha@users.noreply.github.com>
2024-03-25 17:19:32 +0200

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+// function add(){
+//    const sevenBox = document.getElementById("sevenBox");
+//    const sixBox = document.getElementById("sixBox");
+//    const displayBox = document.getElementById("displayBox");
+//    six = sixBox.value;
+//    seven = sevenBox.value;
+//    displayBox.value = six + seven;
+//    alert(six + seven)
+// }
